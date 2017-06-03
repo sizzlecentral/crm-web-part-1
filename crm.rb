76e2c5a2836ebbc4f743e7fe56b09290544d100a
Don Cooper <don_cooper@hotmail.com>
2017-06-02 23:47:57 -0400

    Minor fixes

--- crm.rb
+++ crm.rb
@@ -46,6 +46,15 @@ put '/update_contact/:id' do
   redirect to("/contacts/#{params[:id]}")
 end
 
+get '/contacts/:id' do
+  @contact = Contact.find(params[:id].to_i)
+  if @contact
+    erb :show_contact
+  else
+    raise Sinatra::NotFound
+  end
+end
+
 # ----- End of CRM routes ----- #
 
 after do
